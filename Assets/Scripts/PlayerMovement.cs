2ba11bf74d28d1ff32c20bdbf20a0d2f5560c70f
FoxFate <solembum.pospolity@gmail.com>
2023-09-01 22:06:32 +0200

    Add player movement prototype

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed;
+    [SerializeField] private float maxVelocity = 1f;
+    [SerializeField] private float acceleration = 1f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private GameObject groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     private Rigidbody2D playerRigidBody;
+    private Vector2 moveInput;
 
     private void Awake()
     {
@@ -15,6 +21,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRigidBody.AddForce(Vector2.right *  movementSpeed);
+        playerRigidBody.AddForce((moveInput.x * maxVelocity - playerRigidBody.velocity.x) * acceleration * Vector2.right, ForceMode2D.Force);
+
+        if (Physics2D.OverlapBox(groundCheck.transform.position, groundCheck.transform.localScale, 0, groundLayer))
+        {
+            playerRigidBody.AddForce(moveInput.y * jumpForce * Vector2.up, ForceMode2D.Impulse);
+        }
+
+        if(playerRigidBody.velocity.y < 0)
+        {
+            playerRigidBody.gravityScale = 1.5f;
+        }
+        else
+        {
+            playerRigidBody.gravityScale = 1f;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext value)
+    {
+        moveInput = value.ReadValue<Vector2>();
     }
 }
